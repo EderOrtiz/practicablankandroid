61efe883c852cf5ef3bcfb17281fde93a08d34ea
EderOrtiz <ede.ortiz@duocuc.cl>
2023-08-14 09:43:45 -0400

    mejoras hechas en clases

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {  Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  user1: string = "";
+  nombreUsuario: string = "Ernesto";
+  clave = "1234";
+  lista: any = [
+    {
+      nombre: "Eder",
+      apellido: "Ortiz",
+      edad: 25
+    }
+  ]
 
-  constructor() {}
+  constructor(private router: Router, private alertController: AlertController, private toastController: ToastController) { }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Bienvenido',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Mensaje importantisimo',
+      message: '¡Esto es una alerta!',
+      buttons: ['Continuar'],
+    });
+
+    await alert.present();
+  }
+
+  sumar() {
+    this.clave;
+    console.log("Mostrar mensajes por consola ")
+  }
+
+  inicioSesion(){
+    this.presentToast();
+
+    this.router.navigate(['/pagina1']);
+
+  }
 
 }
+
+
